7bdea9db6264d1d1ac3194ca02913683a12f0f6c
Javiera Vega <javieravegareyes7@gmail.com>
2025-05-27 19:43:05 +0000

    create family static api

--- src/app.py
+++ src/app.py
@@ -8,7 +8,6 @@ from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 # from models import Person
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
@@ -16,28 +15,51 @@ CORS(app)
 # Create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 
 # Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-
 @app.route('/members', methods=['GET'])
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
+    response_body =  members
     return jsonify(response_body), 200
 
+@app.route("/members/<int:the_id>", methods=["GET"])
+def get_one_member(the_id=None):
+    member = jackson_family.get_member(the_id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify("Member not found"), 404
+    
+@app.route("/members", methods=["POST"])
+def create_member():
+    body = request.get_json()
 
+    if type(body) is dict:
+        new_member = jackson_family.add_member(body)
+        
+        if new_member:
+            return jsonify(new_member), 200
+        else:
+            return jsonify("invalid data"), 400
+    
+@app.route("/members/<int:the_id>", methods=["DELETE"])  
+def delete_one_member(the_id):
+    id_delete = jackson_family.delete_member(the_id)
+
+    if id_delete:
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"Message": f"Member con id {the_id} not found"}), 404
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -16,7 +16,22 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
+            
         ]
 
     # This method generates a unique incremental ID
@@ -28,17 +43,45 @@ class FamilyStructure:
     def add_member(self, member):
         ## You have to implement this method
         ## Append the member to the list of _members
-        pass
+        first_name = member.get("first_name")
+        age = member.get("age")
+        lucky_numbers = member.get("lucky_numbers")
+
+        if first_name is None or type(first_name) is not str:
+            return None
+        if age is None or type(age) is not int or age <= 0:
+            return None
+        if lucky_numbers is None or type(lucky_numbers) is not list:
+            return None 
+        
+        new_member = {
+            "id": self._generate_id(),
+            "first_name": first_name,
+            "last_name": self.last_name,
+            "age": age,
+            "lucky_numbers": lucky_numbers
+        }
+        self._members.append(new_member)
+        return new_member
 
     def delete_member(self, id):
         ## You have to implement this method
         ## Loop the list and delete the member with the given id
-        pass
+        array_length = len(self._members)
+        self._members = list(filter(lambda item : item["id"] != id, self._members))
+        if len(self._members) < array_length:
+            return True
+        else:
+            return False
 
     def get_member(self, id):
         ## You have to implement this method
         ## Loop all the members and return the one with the given id
-        pass
+        member = list(filter(lambda item : item["id"] == id, self._members))
+        if member:
+            return member[0]
+        else:
+            return None
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
